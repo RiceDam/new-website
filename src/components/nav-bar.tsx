34fa6b71326078c7d358f2b3ea699056fdefc30b
RiceDam <dam.eric3@gmail.com>
2024-12-27 17:48:39 -0800

    new website project

---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav className={"flex-auto"}>
+      <h1>Eric Dam</h1>
+      <a>Home</a>
+      <a>About</a>
+      <a>Projects</a>
+      <a>Contact</a>
+    </nav>
+  )
+}
+export default Navbar;
